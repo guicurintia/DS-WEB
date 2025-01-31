a78cb577f50b9674f3a60e83d8d96abc3fc9cf26
guicurintia <guilhermeoliveira.g11@gmail.com>
2025-01-31 12:18:42 -0300

    primeiro commit

---
+++ Javascript/AULA-01/readme.md
@@ -0,0 +1,6 @@
+Aula Introdutoria sobre Logica de Progamação com JS
+
+Declaração de Variaveis
+Alert, Confirm, Prompt
+Conversão númerica
+Referencia: Apostila Aula01 de JavaScript
---
+++ Javascript/AULA-01/script.js
@@ -0,0 +1,25 @@
+console.log("Ola Mundo")
+
+//Exemplo de alert()
+alert("Ola Mundo 1")
+//exemplo de confirm()
+var teste = confirm("Ola Mundo 2")
+//exibindo a opção escolhida pelo usuário
+console.log(teste)
+
+//Exemplo de Prompt()
+    var testePrompt = prompt ("digite seu nome")
+    //Exibindo o nome de usuário
+    console.log(testePrompt)
+    alert(testePrompt)
+
+
+    //Tentativa de adição
+    var numero1 = Number.parseInt(prompt("digite o primeiro número"))
+    var numero2 = Number.parseInt(prompt("digite o segundo número"))
+    //verificando o tipo de variável
+    console.log(typeof(numero1))
+    alert(numero1+numero2)
+
+
+   
